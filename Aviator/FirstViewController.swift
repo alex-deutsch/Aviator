6e77506ba876d1687a4170fcd4d98cfe52ef82a6
Alexander Deutsch <mail@alexanderdeutsch.de>
2018-11-17 11:17:08 +0100

    Initial Commit

---
+++ Aviator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Aviator
+//
+//  Created by Alexander Deutsch on 17.11.18.
+//  Copyright © 2018 Alexander Deutsch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Aviator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Aviator
+//
+//  Created by Alexander Deutsch on 17.11.18.
+//  Copyright © 2018 Alexander Deutsch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
